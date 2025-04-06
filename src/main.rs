2b5bcdb711a11a5d6e30a2c341d38b0ee8e77a06
Shaurya Goel <shoray.goel@gmail.com>
2025-04-06 12:44:24 +0530

    Read request line

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{BufRead, BufReader, Read, Write};
 use std::net::TcpListener;
 
 fn main() {
@@ -10,7 +10,21 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                let response = "HTTP/1.1 200 OK\r\n\r\n";
+                // Read the request from the stream
+                let buf_reader = BufReader::new(&_stream);
+                let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+                let mut request_buffer_it = request_line.split_ascii_whitespace();
+
+                let _http_verb = request_buffer_it.next().unwrap_or("GET");
+                let request_target = request_buffer_it.next().unwrap_or("/");
+
+                let response = match request_target {
+                    "/" => "HTTP/1.1 200 OK\r\n\r\n",
+                    _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+                };
+
+                // Write the response to stream
                 _stream.write_all(response.as_bytes()).unwrap();
             }
             Err(e) => {
